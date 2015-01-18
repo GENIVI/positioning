bc371ae975fe9f720f9068f0090fb2acc825d53e
Helmut Schmidt <Helmut.3.Schmidt@continental-corporation.com>
2015-01-18 22:17:03 +0100

    fix generate-doc.sh to run with wkhtmltopdf built against an unpatched version of QT and this not supporting more then one input document

--- enhanced-position-service/doc/tools/generate-doc.sh
+++ enhanced-position-service/doc/tools/generate-doc.sh
@@ -24,6 +24,9 @@ OUTPUT_FILE=$DOC_NAME.pdf
 
 INPUT_FILE1=$DOC_NAME-part1.pdf
 INPUT_FILE2=$DOC_NAME-part2.pdf
+INPUT_FILE3=$DOC_NAME-part3.pdf
+INPUT_FILE4=$DOC_NAME-part4.pdf
+INPUT_FILE5=$DOC_NAME-part5.pdf
 
 #convert xml files into html files
 xsltproc -o ./genivi-positioning-configuration.html           $API_DIR/introspect.xsl    $API_DIR/genivi-positioning-configuration.xml
@@ -32,14 +35,17 @@ xsltproc -o ./genivi-positioning-positionfeedback.html        $API_DIR/introspec
 xsltproc -o ./genivi-positioning-constants.html               $API_DIR/constants.xsl     $API_DIR/genivi-positioning-constants.xml
 
 #concatenate html files into a single pdf file
-wkhtmltopdf ./genivi-positioning-configuration.html ./genivi-positioning-enhancedposition.html ./genivi-positioning-positionfeedback.html ./genivi-positioning-constants.html $INPUT_FILE2
+wkhtmltopdf ./genivi-positioning-configuration.html $INPUT_FILE2
+wkhtmltopdf ./genivi-positioning-enhancedposition.html $INPUT_FILE3
+wkhtmltopdf ./genivi-positioning-positionfeedback.html $INPUT_FILE4
+wkhtmltopdf ./genivi-positioning-constants.html $INPUT_FILE5
 
 #remove temporary html files
 rm *.html
 
 #concatenate pdf files
-pdftk $INPUT_FILE1 $INPUT_FILE2 cat output $OUTPUT_DIR/$OUTPUT_FILE
+pdftk $INPUT_FILE1 $INPUT_FILE2 $INPUT_FILE3 $INPUT_FILE4 $INPUT_FILE5 cat output $OUTPUT_DIR/$OUTPUT_FILE
 
 #remove temporary pdf file
-rm $INPUT_FILE2
+rm $INPUT_FILE2 $INPUT_FILE3 $INPUT_FILE4 $INPUT_FILE5
 
